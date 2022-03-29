a3e05547127d971374c48c3b12e5f79714fb714e
Zpaulis <paulis@zabarovski.lv>
2022-03-29 23:14:27 +0300

    first commit

--- swift_01_lekcija/main.swift
+++ swift_01_lekcija/main.swift
@@ -7,5 +7,41 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Hello, World of Orks!")
+
+let myAge:Int = 55
+var averageAge:Double = 55
+averageAge = (Double(myAge) + 30)/2
+let testNumber = 8
+let evenOdd = 8
+print(averageAge)
+
+let farenheit:Double = 52
+let celcius:Double = (farenheit - 32) / 1.8
+print (celcius)
+
+let age1 = 42
+let age2 = 21
+let avg1 = (Double(age1) + Double(age2))/2
+
+let firstName = "Paulis"
+let lastName = "Zabarovskis"
+let fullName = firstName + " " + lastName
+print ("Sveiki, mani sauc \(fullName)")
+
+var dataTemp = (dd: 29, mm: 4, gggg: 1967, temp: 12.3)
+let (day, _, _, temp) = dataTemp
+dataTemp.temp = 13.25
+let emoji:Character = "\u{1F4A9}"
+print ("\(dataTemp.gggg).gada \(dataTemp.dd).\(dataTemp.mm). ārā bija \(dataTemp.temp) grādi. \(emoji)")
+
+let amIATeeneeger = (myAge < 19) && (myAge > 13)
+
+let reader = fullName
+let author = "Matt Galloway"
+let authorIsReader = reader == author
+let readerAfterAuthor = reader > author
+let answer = (myAge < 19) && (myAge > 13) ? "Teenager" : "Not a teenager"
+print (amIATeeneeger, authorIsReader, readerAfterAuthor, answer)
+
 
